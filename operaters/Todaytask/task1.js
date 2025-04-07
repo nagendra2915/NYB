e5b4fcb40b072da3c9472538e6742ca977c6a090
Nagendra <nagendra.s@nubinfotech.com>
2025-04-07 17:07:00 +0530

    todayTask

---
+++ operaters/Todaytask/task1.js
@@ -0,0 +1,22 @@
+// Grade calculation using if-else ladder
+let marks = 96;
+
+if (marks >= 90) 
+    {
+        console.log("Excellent!");
+    }  
+    else if (marks >= 80)  
+    {
+        console.log("Great Job!");
+    }      
+    else if (marks >= 70) 
+    {
+        console.log("Needs Improvement.");
+    } 
+    else if (marks >= 60) 
+    {
+        console.log("Passed, but Could Do Better.");
+    } 
+    else {
+             console.log("Failed");
+           }
