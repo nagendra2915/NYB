b33d1757107f99dd0cecfd9adf4baae320ffd0cd
Nagendra <nagendra.s@nubinfotech.com>
2025-04-07 16:30:51 +0530

    controlstatements

---
+++ operaters/statements.js
@@ -0,0 +1,25 @@
+// if-else statement
+let age = 20;
+if (age >= 18) 
+    {
+  console.log("You are an adult.");
+}
+else 
+{
+  console.log("You are a minor.");
+}
+
+// if else ladder statement
+
+let A = 10
+if(A == 2)
+{
+    console.log("A value is 2")
+}
+else if(A == 4)
+{
+   console.log("A value is 4")
+}
+else{
+    console.log("A is neither 2 nor 4")
+}
